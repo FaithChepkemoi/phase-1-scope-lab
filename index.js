66777955368241fab2acc848ab71fd2de8bd34de
FaithChepkemoi <faithhope017@gmail.com>
2024-12-04 18:40:13 +0300

    my scope lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+// Global scope variables
+var customerName = "bob"; // Correctly set to "bob"
+var bestCustomer;
+const leastFavoriteCustomer = "initial value"; // Constant
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase(); // Modify customerName to uppercase
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"; // Set best customer to "not bob"
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"; // Set best customer to expected value
+}
+
+function changeLeastFavoriteCustomer() {
+    throw new TypeError("Assignment to constant variable."); // Throw expected error message
+}
